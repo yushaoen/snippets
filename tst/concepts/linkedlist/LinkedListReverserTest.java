49a581841269e3644bce637781bc52234d860c0a
Yu <yushaoen@amazon.com>
2015-06-28 19:17:12 -0700

    added more test cases in LinkedListReverser

--- tst/concepts/linkedlist/LinkedListReverserTest.java
+++ tst/concepts/linkedlist/LinkedListReverserTest.java
@@ -8,12 +8,34 @@ import junit.framework.TestCase;
 public class LinkedListReverserTest extends TestCase {
 
     public void testReverse() throws Exception {
-        Node root = new Node<Integer>(1,
-                                      new Node<Integer>(2,
-                                                        new Node<Integer>(3,
-                                                                          new Node<Integer>(4,
-                                                                                            null))));
-        Node node = LinkedListReverser.reverse(root);
+
+        Node root, node;
+
+        /**
+         * Null case
+         */
+        assertTrue(LinkedListReverser.reverse(null) == null);
+
+        /**
+         * 1-Node case
+         */
+        root = new Node<Integer>(1, null);
+
+        node = LinkedListReverser.reverse(root);
+
+        assertEquals(1, node.data);
+        node = node.next;
+        assertTrue(node == null);
+
+        /**
+         * Multi-Node case
+         */
+        root = new Node<Integer>(1,
+                                 new Node<Integer>(2,
+                                                   new Node<Integer>(3,
+                                                                     new Node<Integer>(4,
+                                                                                       null))));
+        node = LinkedListReverser.reverse(root);
         assertEquals(4, node.data);
         node = node.next;
         assertEquals(3, node.data);
